0ff99e3df38b37de1bc3d62b5313c30eac3a6293
Jan Wiemer <jan.wiemer@online.de>
2015-02-06 20:46:43 +0100

    fixed javadoc

--- src/main/java/org/jacis/store/StoreEntryInfo.java
+++ src/main/java/org/jacis/store/StoreEntryInfo.java
@@ -6,7 +6,8 @@ package org.jacis.store;
  * State information regarding a store entry
  *
  * @param <K> Key type of the store entry
- * @param <V> Value type of the store entry
+ * @param <TV> Type of the objects in the transaction view. This is the type visible from the outside.
+ * @param <CV> Type of the objects as they are stored in the internal map of committed values. This type is not visible from the outside.
  */
 public class StoreEntryInfo<K, TV, CV> {
 
