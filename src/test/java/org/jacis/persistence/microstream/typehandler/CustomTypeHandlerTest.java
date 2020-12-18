0eee05582f1af76534074510ab8804890445cda5
Jan Wiemer <jan.wiemer@online,.de>
2020-12-18 20:52:33 +0100

    First tests with microstream custom type handlers

--- src/test/java/org/jacis/persistence/microstream/typehandler/CustomTypeHandlerTest.java
+++ src/test/java/org/jacis/persistence/microstream/typehandler/CustomTypeHandlerTest.java
@@ -1,5 +1,6 @@
 package org.jacis.persistence.microstream.typehandler;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
@@ -31,8 +32,8 @@ public class CustomTypeHandlerTest {
   }
 
   @Test
-  public void testCustomTyopeHandler() {
-    Path storageDir = getStorageDir("testCustomTyopeHandler");
+  public void testCustomTypeHandler() {
+    Path storageDir = getStorageDir("testCustomTypeHandler");
     EmbeddedStorageManager storageManager = createStorageManager(storageDir);
     assertNull(storageManager.root());
     storageManager.setRoot(new TestObject("Jacis", "Store"));
@@ -47,10 +48,46 @@ public class CustomTypeHandlerTest {
     storageManager.shutdown();
   }
 
+  @Test
+  public void testCustomListTypeHandler() {
+    Path storageDir = getStorageDir("testCustomListTypeHandler");
+    EmbeddedStorageManager storageManager = createStorageManager(storageDir);
+    assertNull(storageManager.root());
+    TestListObject list = new TestListObject(5);
+    storageManager.setRoot(list);
+    list.add("One");
+    list.add("Two");
+    list.add("Three");
+    log.info("TEST list: {}",list);
+    storageManager.storeRoot();
+    list.getContent().forEach(e->storageManager.store(e));
+    storageManager.shutdown();
+    EmbeddedStorageManager storageManager2 = createStorageManager(storageDir);
+    TestListObject checkList1 = (TestListObject) storageManager2.root();
+    log.info("TEST check 1: {}",checkList1);
+    assertEquals(3, checkList1.getContent().size());
+    assertEquals("One", checkList1.getContent().get(0));
+    assertEquals("Two", checkList1.getContent().get(1));
+    assertEquals("Three", checkList1.getContent().get(2));
+//    checkList1.getContent().set(0, "One New ILLEGAL");
+    checkList1.set(1, "Two New");
+    storageManager2.storeRoot();
+    storageManager2.shutdown();
+    EmbeddedStorageManager storageManager3 = createStorageManager(storageDir);
+    TestListObject checkList2 = (TestListObject) storageManager3.root();
+    log.info("TEST check 2: {}",checkList2);
+    assertEquals(3, checkList2.getContent().size());
+    assertEquals("One", checkList2.getContent().get(0));
+    assertEquals("Two New", checkList2.getContent().get(1));
+    assertEquals("Three", checkList2.getContent().get(2));
+    storageManager3.storeRoot();
+    storageManager3.shutdown();
+  }
+
   @SuppressWarnings("unchecked")
   protected EmbeddedStorageManager createStorageManager(Path storageDir) {
     return EmbeddedStorage.Foundation(storageDir) //
-        .onConnectionFoundation(f -> f.registerCustomTypeHandlers(new TestObjectHandler())) //
+        .onConnectionFoundation(f -> f.registerCustomTypeHandlers(new TestObjectHandler(),new TestListObjectHandler())) //
         .start();
   }
 
---
+++ src/test/java/org/jacis/persistence/microstream/typehandler/TestListObjectHandler.java
@@ -0,0 +1,83 @@
+package org.jacis.persistence.microstream.typehandler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import one.microstream.persistence.binary.internal.AbstractBinaryHandlerCustom;
+import one.microstream.persistence.binary.types.Binary;
+import one.microstream.persistence.types.PersistenceLoadHandler;
+import one.microstream.persistence.types.PersistenceReferenceLoader;
+import one.microstream.persistence.types.PersistenceStoreHandler;
+
+public class TestListObjectHandler extends AbstractBinaryHandlerCustom<TestListObject> {
+
+  private static final long BINARY_OFFSET_BASE = 0;
+  private static final long BINARY_OFFSET_SIZE = BINARY_OFFSET_BASE;
+  private static final long BINARY_OFFSET_ELEMENTS = BINARY_OFFSET_SIZE + Integer.BYTES;
+
+  private static final Logger log = LoggerFactory.getLogger(TestListObjectHandler.class);
+
+  public TestListObjectHandler() {
+    super(TestListObject.class, SimpleArrayFields(CustomField(int.class, "size")));
+    log.info("instantiated: {} (-> {})", this.getClass(), this);
+  }
+
+  @Override
+  public void store(Binary data, TestListObject instance, long objectId, PersistenceStoreHandler<Binary> handler) {
+    log.info("store(.., {}, {}, ...) (data: {}, handler: {})", instance, objectId, data, handler);
+    int size = instance.getContent().size();
+    long binarySize = BINARY_OFFSET_ELEMENTS - BINARY_OFFSET_BASE + Binary.referenceBinaryLength(size);
+    data.storeEntityHeader(binarySize, this.typeId(), objectId);
+    data.store_int(BINARY_OFFSET_SIZE, size);
+    for (int i = 0; i < size; i++) {
+//    for (Integer i : instance.getModified()) {
+      String elem = instance.getContent().get(i);
+      long elementObjectId = handler.apply(elem);
+      log.info(" ... store modified object[{}] {} -> {}", i, elementObjectId, elem);
+      data.store_long(BINARY_OFFSET_ELEMENTS + Binary.referenceBinaryLength(i), elementObjectId);
+    }
+    instance.getModified().clear();
+  }
+
+  @Override
+  public TestListObject create(Binary data, PersistenceLoadHandler handler) {
+    log.info("start create(..., ...) (data: {}, handler: {})", data, handler);
+    int size = data.read_int(BINARY_OFFSET_SIZE);
+    TestListObject res = new TestListObject(size);
+    log.info("create(..., ...) -> {} ", res);
+    return res;
+  }
+
+  @Override
+  public void updateState(Binary data, TestListObject instance, PersistenceLoadHandler handler) {
+    log.info("updateState(..., {},...) -> (data: {}, handler: {})", instance, data, handler);
+    int size = data.read_int(BINARY_OFFSET_SIZE);
+    for (int i = 0; i < size; i++) {
+      long objectId = data.read_long(BINARY_OFFSET_ELEMENTS + Binary.referenceBinaryLength(i));
+      log.info(" ... load object {}", objectId);
+      instance.getContent().add((String) handler.lookupObject(objectId));
+    }
+  }
+
+  @Override
+  public void iterateLoadableReferences(Binary data, PersistenceReferenceLoader iterator) {
+    log.info("iterateLoadableReferences(...,...) -> (data: {}, iterator: {})", data, iterator);
+    int size = data.read_int(BINARY_OFFSET_SIZE);
+    for (int i = 0; i < size; i++) {
+      long objectId = data.read_long(BINARY_OFFSET_ELEMENTS + Binary.referenceBinaryLength(i));
+      log.info(" ... register object {}", objectId);
+      iterator.acceptObjectId(objectId);
+    }
+  }
+
+  @Override
+  public boolean hasPersistedReferences() {
+    return true;
+  }
+
+  @Override
+  public boolean hasVaryingPersistedLengthInstances() {
+    return true;
+  }
+
+}
